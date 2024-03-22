bd8fcdff29cd827b50e1193fd4b752e5a0365c8f
shadowgks <saad.moumou.coder@gmail.com>
2024-03-22 23:21:08 +0000

    Update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modules/dashboard/components/modals/modal-actor/modal-actor.component';
 import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 // import function to register Swiper custom elements
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -10,6 +12,8 @@ import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
     
   ],
   imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
